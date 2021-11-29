506ac485c97b738fd958dcf3dfaa3f1a38bf824f
JamesDradge <bobdradge@hotmail.com>
2021-11-29 16:31:09 +0000

    Finished first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString;
+    if (num > 0) {
+        repeatedString = string;
+        for (i = 0; (i < num -1); i++){
+            repeatedString += string;
+        }
+    } else if (num < 0) {
+        repeatedString = "ERROR";
+    } else {
+        repeatedString = "";
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
