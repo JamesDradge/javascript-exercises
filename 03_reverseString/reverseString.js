55b8129eca27924cafad6ff40915ea1ceed12569
JamesDradge <bobdradge@hotmail.com>
2021-11-29 16:48:09 +0000

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,27 @@
 const reverseString = function(str) {
 
+    // Replace spaces between words with hyphen
+    str = str.replace(/ /g, "-");
+
+    // Convert to array
+    let arrayString = str.split('');
+
+    //Reverse array
+    arrayString = arrayString.reverse();
+
+    // Convert back to string
+    let reversedString = arrayString.toString();
+
+
+    //remove array formatting
+    reversedString = reversedString.replace(/,/g, "");
+    reversedString = reversedString.replace(/ /g, "");
+
+    //Add back in spaces between words
+    reversedString = reversedString.replace(/-/g, " ");
+
+    //Return final result
+    return reversedString;
 };
 
 // Do not edit below this line
