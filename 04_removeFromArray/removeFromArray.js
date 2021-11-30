56a55601848a859509202d898ce7c5b0d268c527
JamesDradge <bobdradge@hotmail.com>
2021-11-30 19:30:30 +0000

    Finished exercise 4 - had to look at the solution and then spend some time understanding it, couldnt figure out how to get my attempt to work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function(array, num) {
+const removeFromArray = function(...args) {
+    // for (i = 0; i < array.length; i++){
+    //     if (array[i] != args.includes(i)){
+    //         newArray.push(array[i]);
+    //     } 
+    // }
+    // return newArray;
+    const array = args[0];
+    const newArray = [];
 
-};
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
