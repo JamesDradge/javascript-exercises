99ab75e701aa26d17b12ab84c2fff82b3fc66ad9
JamesDradge <bobdradge@hotmail.com>
2021-11-30 20:24:34 +0000

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let startNumber;
+    let endNumber;
+    
+    if (num1 > 0 && num2 > 0 && typeof num1 == "number" && typeof num2 == "number"){
+        if (num1 < num2) {
+            startNumber = num1;
+            endNumber = num2;
+        } else {
+            startNumber = num2;
+            endNumber = num1;
+        }
 
+        let sum = startNumber;
+        while (startNumber < endNumber) {
+            sum = sum + startNumber + 1;
+            startNumber++;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
