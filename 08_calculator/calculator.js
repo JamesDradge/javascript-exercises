375b5d8f29a0e589d387a035064611065b11a3ff
JamesDradge <bobdradge@hotmail.com>
2021-12-15 21:48:31 +0000

    Finished ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  const reducer = (previousValue, currentValue) =>  previousValue + currentValue;
+  return a.reduce(reducer, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  const reducer = (previousValue, currentValue) => previousValue * currentValue;
+  return a.reduce(reducer, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for (a; a > 0; a--) {
+    fact *= a;
+  }
+  return fact;
 };
 
 // Do not edit below this line
