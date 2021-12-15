51a2d8a020a9fe2c6bbdaa5badc9aab00f7a600b
JamesDradge <bobdradge@hotmail.com>
2021-12-15 22:12:46 +0000

    Solved this in a super janky feeling way but... finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.trim().toLowerCase().replace(/\W|_/g, '');
 
+    let arrayWord = word.substring(0);
+    for (let i = 0; i < (arrayWord.length/2); i++) {
+        if (arrayWord[i] !== arrayWord[(arrayWord.length -i -1)]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 };
 
 // Do not edit below this line
