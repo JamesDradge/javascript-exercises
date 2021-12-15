33b97b6bc82ada9430138cbc284c91d1407722be
JamesDradge <bobdradge@hotmail.com>
2021-12-15 22:57:40 +0000

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    let firstFib = 0;
+    let secondFib = 1;
 
+    while (num > 0) {
+        let temp = secondFib;
+        secondFib = firstFib + secondFib; 
+        firstFib = temp; 
+        num--;
+    }
+    return firstFib;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// > A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+
+// ```javascript
+// fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
+// fibonacci(6) // returns 8
+// ```
