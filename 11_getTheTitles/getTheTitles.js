6648b671fb6108ef7f35100b2860de28acdb9395
JamesDradge <bobdradge@hotmail.com>
2021-12-16 08:21:05 +0000

    Finished ex 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    return books.map(book => book.title);
+    
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
