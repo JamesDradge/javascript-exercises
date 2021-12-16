e7530b1c2820e94b86e6bcd6763012d85704e67f
JamesDradge <bobdradge@hotmail.com>
2021-12-16 08:44:49 +0000

    Finished ex 12, needed to google for some help though

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth); 
+        const currentAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        if (currentAge > oldestAge) {
+            return currentPerson;
+        } else {
+            return oldest;
+        }
+    });
 };
 
+function getAge(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
+
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ]
